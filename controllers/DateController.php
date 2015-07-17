e42466719d59435523f17f290e39ef56396c4b75
星文 <woandni1314@163.com>
2015-07-17 15:54:05 +0800

    实现预约Action功能

--- controllers/DateController.php
+++ controllers/DateController.php
@@ -7,14 +7,32 @@
  */
 
 namespace app\controllers;
-
-use app\controllers\Controller;
+use app\models\Interview;
 
 class DateController extends Controller
 {
     public function actionSetting() {
-        $this->view->title = '360校园招聘-选择日期和地点';
-        return $this->render('setting');
+        //$data=Yii::$app->request->post();
+        $data = array(
+            'interview_id' => '面试序号',
+            'user_id' => '面试者序号',
+            'interview_city' => '城市',
+            'interview_date' => '面试日期',
+            'interview_time' => '面试开始时间',
+            'create_time' => '创建时间',
+            'export_time' => '导出时间',
+            'notified' => '通知时间',
+            'status' => '预约状态',
+        );
+        $model = new Interview();
+        //判断POST和保存是否成功，成功跳转到成功页，不成功则返回选择页
+        if ($model->load($data) && $model->save()) {
+            return $this->redirect('success');
+        } else {
+            return $this->render('setting', [
+                'model' => $model,
+            ]);
+        }
     }
     public function actionConfirm() {
         return $this->render('confirm');
